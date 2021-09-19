e21234264d28a3031cbfd5c3abbd3f2503666d9d
matsouto <matsouto55@gmail.com>
2021-09-19 19:23:25 -0300

    Remove nav,status and action bars

--- app/src/main/java/com/souto/rcircuitcalc/MainActivity.java
+++ app/src/main/java/com/souto/rcircuitcalc/MainActivity.java
@@ -3,12 +3,26 @@ package com.souto.rcircuitcalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // This would remove the navigation and status bar
+        //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION); // Navigation
+        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN); // Status
+
+        // The next 2 lines remove the status bar on top of the screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        getSupportActionBar().hide(); // Removes the action bar on top
+
         setContentView(R.layout.activity_main);
     }
 }
